3a938723529bd149b9db4a6a56aa1f7722efc03f
Ciara Weets <122639850+cweets@users.noreply.github.com>
2023-08-17 17:50:40 -0400

    Add files via upload
    Here is code for a preliminary portion of a figure. Please enjoy, but be warned, it's ugly!

---
+++ WaterQuality.R
@@ -0,0 +1,22 @@
+#Map of National Level Water Quality Legislation by Country from AMP for EID#
+
+library(readxl)
+AMR_Water_Quality <- read_excel("AMR Water Quality.xls")
+
+world <- map_data("world")
+world_data <- inner_join(world, AMR_Water_Quality, by = join_by(region == Country))
+
+ggplot(data = world_data, mapping = aes(x = long, y = lat, group = group)) + 
+  coord_fixed(1.3) +
+  geom_polygon(aes(fill = Status)) +
+  ggtitle("Water Quality Legislation") +
+  labs(fill = "Status") +
+  theme(
+    axis.text = element_blank(),
+    axis.line = element_blank(),
+    axis.ticks = element_blank(),
+    panel.border = element_blank(),
+    panel.grid = element_blank(),
+    axis.title = element_blank(),
+    panel.background = element_rect(fill),
+    plot.title = element_text(hjust = 0.5))
