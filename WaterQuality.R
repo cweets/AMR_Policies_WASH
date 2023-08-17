d497f733c1fd7a95c77ce016e78a4f9b685d93f3
Ciara Weets <122639850+cweets@users.noreply.github.com>
2023-08-17 18:35:18 -0400

    Add files via upload
    Version 2. Took off the scale. I vibe with the pink and green, actually.

--- WaterQuality.R
+++ WaterQuality.R
@@ -10,13 +10,17 @@ ggplot(data = world_data, mapping = aes(x = long, y = lat, group = group)) +
   coord_fixed(1.3) +
   geom_polygon(aes(fill = Status)) +
   ggtitle("Water Quality Legislation") +
+  scale_fill_manual(values = wes_palette("Moonrise3")) +
   labs(fill = "Status") +
   theme(
+    legend.position = "none",
     axis.text = element_blank(),
     axis.line = element_blank(),
     axis.ticks = element_blank(),
     panel.border = element_blank(),
     panel.grid = element_blank(),
     axis.title = element_blank(),
-    panel.background = element_rect(fill),
+    panel.background = element_rect(fill= "white"),
     plot.title = element_text(hjust = 0.5))
+
+  
