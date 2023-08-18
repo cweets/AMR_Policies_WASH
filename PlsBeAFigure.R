c1410254a438631c360a99878eadf27aa9552508
Ciara Weets <122639850+cweets@users.noreply.github.com>
2023-08-18 14:13:29 -0400

    Add files via upload
    Very pleased with myself. Colors need to be changed and it needs a legend. Patchworked the figure together.

---
+++ PlsBeAFigure.R
@@ -0,0 +1,165 @@
+#Make Monitoring Map#
+
+library(readxl)
+AMR_Water_Monitor <- read_excel("AMR Water Monitoring.xls")
+
+world <- map_data("world")
+world_data <- inner_join(world, AMR_Water_Monitor, by = join_by(region == Country))
+
+MonMap <- (ggplot(data = world_data, mapping = aes(x = long, y = lat, group = group)) + 
+             coord_fixed(1.3) +
+             geom_polygon(aes(fill = Status)) +
+             ggtitle("Water Contamination Monitoring") +
+             scale_fill_manual(values = wes_palette("Moonrise3")) +
+             labs(fill = "Status") +
+             theme(
+               legend.position = "none",
+               axis.text = element_blank(),
+               axis.line = element_blank(),
+               axis.ticks = element_blank(),
+               panel.border = element_blank(),
+               panel.grid = element_blank(),
+               axis.title = element_blank(),
+               panel.background = element_rect(fill= "white"),
+               plot.title = element_text(hjust = 0.5)))
+
+#Make Quality Map#
+
+library(readxl)
+AMR_Water_Quality <- read_excel("AMR Water Quality.xls")
+
+world <- map_data("world")
+world_data <- inner_join(world, AMR_Water_Quality, by = join_by(region == Country))
+
+QualMap <- (
+  ggplot(data = world_data, mapping = aes(x = long, y = lat, group = group)) + 
+  coord_fixed(1.3) +
+  geom_polygon(aes(fill = Status)) +
+  ggtitle("Water Quality Legislation") +
+  scale_fill_manual(values = wes_palette("Moonrise3")) +
+  labs(fill = "Status") +
+  theme(
+    legend.position = "none",
+    axis.text = element_blank(),
+    axis.line = element_blank(),
+    axis.ticks = element_blank(),
+    panel.border = element_blank(),
+    panel.grid = element_blank(),
+    axis.title = element_blank(),
+    panel.background = element_rect(fill= "white"),
+    plot.title = element_text(hjust = 0.5)))
+
+#Make Pollution Map
+
+library(readxl)
+AMR_Water_Pollutant <- read_excel("AMRPollutantDisposal.xls")
+
+world <- map_data("world")
+world_data <- inner_join(world, AMR_Water_Pollutant, by = join_by(region == Country))
+
+PollMap <- (
+  ggplot(data = world_data, mapping = aes(x = long, y = lat, group = group)) + 
+              coord_fixed(1.3) +
+              geom_polygon(aes(fill = Status)) +
+              ggtitle("Water Pollutant Disposal Regulations") +
+              scale_fill_manual(values = wes_palette("Moonrise3")) +
+              labs(fill = "Status") +
+              theme(
+                legend.position = "none",
+                axis.text = element_blank(),
+                axis.line = element_blank(),
+                axis.ticks = element_blank(),
+                panel.border = element_blank(),
+                panel.grid = element_blank(),
+                axis.title = element_blank(),
+                panel.background = element_rect(fill= "white"),
+                plot.title = element_text(hjust = 0.5)))
+
+#Make Effluent Map
+
+library(readxl)
+AMR_Water_Effluent <- read_excel("AMREffluentDisposal.xls")
+
+world <- map_data("world")
+world_data <- inner_join(world, AMR_Water_Effluent, by = join_by(region == Country))
+
+EffMap <- (
+  ggplot(data = world_data, mapping = aes(x = long, y = lat, group = group)) + 
+  coord_fixed(1.3) +
+  geom_polygon(aes(fill = Status)) +
+  ggtitle("Wastewater Effluent Disposal Regulation") +
+  scale_fill_manual(values = wes_palette("Moonrise3")) +
+  labs(fill = "Status") +
+  theme(
+    legend.position = "none",
+    axis.text = element_blank(),
+    axis.line = element_blank(),
+    axis.ticks = element_blank(),
+    panel.border = element_blank(),
+    panel.grid = element_blank(),
+    axis.title = element_blank(),
+    panel.background = element_rect(fill= "white"),
+    plot.title = element_text(hjust = 0.5)))
+
+#Make Sewer Map
+
+library(readxl)
+AMR_Sewer <- read_excel("AMRSewer.xls")
+
+world <- map_data("world")
+world_data <- inner_join(world, AMR_Sewer, by = join_by(region == Country))
+
+SewMap <- (
+  ggplot(data = world_data, mapping = aes(x = long, y = lat, group = group)) + 
+  coord_fixed(1.3) +
+  geom_polygon(aes(fill = Status)) +
+  ggtitle("Sewerage Regulations") +
+  scale_fill_manual(values = wes_palette("Moonrise3")) +
+  labs(fill = "Status") +
+  theme(
+    legend.position = "none",
+    axis.text = element_blank(),
+    axis.line = element_blank(),
+    axis.ticks = element_blank(),
+    panel.border = element_blank(),
+    panel.grid = element_blank(),
+    axis.title = element_blank(),
+    panel.background = element_rect(fill= "white"),
+    plot.title = element_text(hjust = 0.5)))
+
+#Make Medical Waste Map
+
+library(readxl)
+AMR_Med_Waste <- read_excel("AMRMedWaste.xls")
+
+world <- map_data("world")
+world_data <- inner_join(world, AMR_Med_Waste, by = join_by(region == Country))
+
+MedMap <- (
+  ggplot(data = world_data, mapping = aes(x = long, y = lat, group = group)) + 
+  coord_fixed(1.3) +
+  geom_polygon(aes(fill = Status)) +
+  ggtitle("Medical Waste Disposal Regulations") +
+  scale_fill_manual(values = wes_palette("Moonrise3")) +
+  labs(fill = "Status") +
+  theme(
+    legend.position = "none",
+    axis.text = element_blank(),
+    axis.line = element_blank(),
+    axis.ticks = element_blank(),
+    panel.border = element_blank(),
+    panel.grid = element_blank(),
+    axis.title = element_blank(),
+    panel.background = element_rect(fill= "white"),
+    plot.title = element_text(hjust = 0.5)))
+
+#Open patchwork from library
+library(patchwork)
+
+#Put all of the Maps into the patchwork plot
+
+MapsFigure <- (
+  MonMap + QualMap + PollMap + EffMap + SewMap + MedMap + plot_layout(nrow = 3, byrow = FALSE))
+
+print(MapsFigure)
+
